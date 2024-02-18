7529b5bb9383c3334556f66b85f0bc1a12fa579b
evgeniiprotsenko <procenko.zheka@gmail.com>
2024-02-19 01:57:32 +0500

    hw1

--- bot/src/main/java/edu/java/bot/service/CommandService.java
+++ bot/src/main/java/edu/java/bot/service/CommandService.java
@@ -10,12 +10,12 @@ public class CommandService {
     }
 
     public String helpCommand() {
-        return "Список команд:\n" +
-            "/start - зарегистрировать пользователя\n" +
-            "/help - вывести окно с командами\n" +
-            "/track - начать отслеживание ссылки\n" +
-            "/untrack - прекратить отслеживание ссылки\n" +
-            "/list - показать список отслеживаемых ссылок";
+        return "Список команд:\n"
+            + "/start - зарегистрировать пользователя\n"
+            + "/help - вывести окно с командами\n"
+            + "/track - начать отслеживание ссылки\n"
+            + "/untrack - прекратить отслеживание ссылки\n"
+            + "/list - показать список отслеживаемых ссылок";
     }
 
     public String trackCommand(String link) {
@@ -30,8 +30,8 @@ public class CommandService {
 
     public String listCommand() {
         // Здесь будет логика обработки команды /list
-        return "Список отслеживаемых ссылок:\n" +
-            "https://github.com/example1\n" +
-            "https://stackoverflow.com/example2";
+        return "Список отслеживаемых ссылок:\n"
+            + "https://github.com/example1\n"
+            + "https://stackoverflow.com/example2";
     }
 }
